8210660ab9f07cd444755601d4ac3357dbb29667
Amanda093 <vovigadagamer@gmail.com>
2025-04-15 08:38:01 -0300

    add state to formcriartarefa

--- src/components/FormCriarTarefa/FormCriarTarefa.jsx
+++ src/components/FormCriarTarefa/FormCriarTarefa.jsx
@@ -1,12 +1,40 @@
-import { Botao, CampoTexto  } from "../../components";
-import style from "./FormCriarTarefa.module.css"
+import { useState } from "react";
+import { Botao, CampoTexto } from "../../components";
+import style from "./FormCriarTarefa.module.css";
 
-const FormCriarTarefa = () => {
-    return ( 
-    <form className={style.FormCriarTarefa} action="">
-        <CampoTexto />
-        <Botao texto="+"/>
-    </form> );
-}
- 
+const FormCriarTarefa = (props) => {
+  const [nomeTarefa, setNomeTarefa] = useState("");
+
+  const { setTarefas } = props;
+
+  const onChangeNomeTarefa = (event) => {
+    setNomeTarefa(event.currentTarget.value);
+  };
+
+  const adicionarTarefa = (event) => {
+    event.preventDefault(); // para o navegador não recarregar
+
+    if (!nomeTarefa) {
+      return;
+    }
+
+    setTarefas((estadoAtual) => {
+      const tarefa = {
+        id: estadoAtual.length + 1,
+        nome: nomeTarefa,
+      };
+
+      return [...estadoAtual, tarefa];
+    });
+    setNomeTarefa("");
+  };
+
+  return (
+    <form className={style.FormCriarTarefa} onSubmit={adicionarTarefa}>
+      <CampoTexto value={nomeTarefa} onChange={onChangeNomeTarefa} />
+      <Botao texto="+" />
+    </form>
+  );
+};
+
 export { FormCriarTarefa };
